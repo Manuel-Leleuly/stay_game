286711f7c9244730f1b49b2fe1f6afead5f0f0f4
Manuel <manuel.leleuly@gmail.com>
2020-12-06 18:58:53 +0700

    again, so far so good

--- stay2/lib/main.dart
+++ stay2/lib/main.dart
@@ -5,17 +5,39 @@ import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:stay/stay-game.dart';
 
+SharedPreferences storage;
+StayGame game;
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   Util stayUtil = Util();
   await stayUtil.fullScreen();
   await stayUtil.setOrientation(DeviceOrientation.portraitUp);
 
-  SharedPreferences storage = await SharedPreferences.getInstance();
-  StayGame game = StayGame(storage);
-  runApp(game.widget);
+  // SharedPreferences storage = await SharedPreferences.getInstance();
+  // StayGame game = StayGame(storage);
+  // runApp(game.widget);
+  storage = await SharedPreferences.getInstance();
+  game = StayGame(storage);
+  runApp(RunApp());
 
   TapGestureRecognizer tapper = TapGestureRecognizer();
   tapper.onTap = game.onTap;
   stayUtil.addGestureRecognizer(tapper);
 }
+
+class RunApp extends StatefulWidget {
+  @override
+  _RunAppState createState() => _RunAppState();
+}
+
+class _RunAppState extends State<RunApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: game.widget,
+    );
+  }
+}
+
